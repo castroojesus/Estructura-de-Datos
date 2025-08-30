65f0bc724d3b38d5a463aa0eb02cc8fec4225aad
castroojesus <jesusacs1111@gmail.com>
2025-08-29 22:06:32 -0700

    Create EjercicioArray.java
    creacion del ejemplo de arreglos en Java

---
+++ EjercicioArray.java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+        String[] miArray={"David", "Jorge", "Carro"};
+        for(int i=0; i<miArray.length; i++){
+            System.out.println(miArray[i]);
+        }
+    }
+}
