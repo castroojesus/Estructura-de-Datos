074c4c834782395c1b0cfd62d58f070fdcbfc518
castroojesus <jesusacs1111@gmail.com>
2025-09-01 23:10:20 -0700

    Create Recorrido.cpp

---
+++ RECORRIDO, BUSQUEDA Y INSERCION/Recorrido.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int miArray[]={1,2,3,4,5};
+  cout<<"Los elementos del array son:"<<endl;
+  for(int i=0;i<5;i++){
+    cout<<miArray[i];
+    cout<<" "<<endl;
+  }
+
+
+    return 0;
+}
