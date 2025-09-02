fef040b03df8ae3427627840ee3b525f52c91cb0
castroojesus <jesusacs1111@gmail.com>
2025-09-01 20:55:23 -0700

    creacion de recorrido en java

---
+++ Recorrido.java
@@ -0,0 +1,12 @@
+public class Recorrido {
+    public static void main(String[] args) {
+        
+        int[] miArray = {1,2,3,4,5};
+        System.out.println("Los elementos del array son: ");
+        for(int i=0;i<5;i++){
+            System.out.println(miArray[i]);
+            
+        }
+        
+    }
+}
