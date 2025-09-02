3a29216e5bcedf61dd38c546958d9b74f97fda5f
castroojesus <jesusacs1111@gmail.com>
2025-09-01 23:00:29 -0700

    Create Recorrido.cs

---
+++ RECORRIDO, BUSQUEDA Y INSERCION/Recorrido.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Recorrido
+{
+    public static void Main(string[] args){
+        
+        int[] miArray={1,2,3,4,5};
+        Console.WriteLine("Los elementos del array son: ");
+        for(int i=0;i<5;i++){
+            Console.WriteLine(miArray[i]);
+        }
+        
+    }
+}
