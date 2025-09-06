f502663db409d5c5d028cbf240fab58d82a3338e
castroojesus <jesusacs1111@gmail.com>
2025-09-05 19:01:28 -0700

    Update Filas.java

---
+++ Filas.java
@@ -0,0 +1,36 @@
+public class Filas {
+    public static void main(String[] args) {
+        final int r=3;
+        final int c=3;
+
+        int[] arr=new int[r*c];
+        int[][] TwoDArray={{1,2,3},{4,5,6},{7,8,9}};
+        int k=0;
+
+        
+        for(int x=0;x<r;x++){
+            for(int y=0;y<c;y++){
+                k=x*r+y;
+                arr[k]=TwoDArray[x][y];
+                k=k+1;
+            }
+        }
+        System.out.println("Los elementos del array bidimensional son: ");
+         for(int x=0;x<r;x++){
+            for(int y=0;y<c;y++){
+                System.out.println(TwoDArray[x][y]+" ");
+            }
+            
+        }
+
+        System.out.println("\nLos elementos del array unidimensional son: ");
+        for(int x=0;x<r;x++){
+            for(int y=0;y<c;y++){
+                System.out.println(arr[x*r+y]+" ");
+            }
+                
+        }
+
+    }
+    
+}
