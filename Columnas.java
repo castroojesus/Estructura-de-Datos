4611bc761cb3b718710e508d5fe13512f15d9954
castroojesus <jesusacs1111@gmail.com>
2025-09-08 21:32:22 -0700

    Update Columnas.java

--- Columnas.java
+++ Columnas.java
@@ -1,20 +1,10 @@
- public class Columnas {
+  public class Columnas {
     public static void main(String[] args) {
         final int r=3;
         final int c=3;
-
-        int[] arr=new int[r*c];
-        int[][] TwoDArray={{1,2,3},{4,5,6},{7,8,9}};
-        int k=0;
-
         
-        for(int y=0;y<c;y++){
-            for(int x=0;x<r;x++){
-                k=y*r+x;
-                arr[k]=TwoDArray[x][y];
-                k=k+1;
-            }
-        }
+        int[][] TwoDArray={{1,2,3},{4,5,6},{7,8,9}};       
+
         System.out.println("Los elementos del array bidimensional son: ");
          for(int x=0;x<r;x++){
             for(int y=0;y<c;y++){
@@ -26,7 +16,7 @@
         System.out.println("\nLos elementos del array unidimensional son: ");
         for(int y=0;y<c;y++){
             for(int x=0;x<r;x++){
-                System.out.println(arr[y*r+x]+" ");
+                System.out.println(TwoDArray[x][y]+" ");
             }
            
         }
