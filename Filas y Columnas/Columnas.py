b7f46e3da8d165d7a936f58ac104f1436db413ee
castroojesus <jesusacs1111@gmail.com>
2025-09-08 21:27:29 -0700

    Update Columnas.py

--- Filas y Columnas/Columnas.py
+++ Filas y Columnas/Columnas.py
@@ -1,17 +1,16 @@
-r=3;c=3
-arr=[0]*r*c
 TwoDArr=[[1,2,3],[4,5,6],[7,8,9]];
-k=0
-for y in range(c): #inicio con las columnas y luego con las filas
-    for x in range(r):
-        k=y*r+x; #la formula cambia 
-        arr[k]=TwoDArr[x][y]
-        k=k+1
+
+
 print("Los elementos del array bidimensional son: ")
 for row in TwoDArr:
     for ele in row:
         print(ele,end=" ")
     print()
+
+filas=len(TwoDArr)
+columnas=len(TwoDArr[0])
+
 print("\nLos elementos del array unidimensional son: ")
-for x in range(len(arr)):
-    print(arr[x],end=" ")
+for y in range(columnas):
+    for x in range(filas):
+        print((TwoDArr[x][y]),end=" ")
