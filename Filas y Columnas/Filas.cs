6f6f05a3302cdcaf4e620c2f14f15a1e11223f50
castroojesus <jesusacs1111@gmail.com>
2025-09-08 21:30:53 -0700

    Update Filas.cs

--- Filas y Columnas/Filas.cs
+++ Filas y Columnas/Filas.cs
@@ -6,19 +6,11 @@ class Program
     {
         int r = 3;
         int c = 3;
-        int[] arr = new int[r * c];
+        
         int[,] TwoDArray = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        int k = 0;
+        
 
-        for (int x = 0; x < r; x++)
-        {
-            for (int y = 0; y < c; y++)
-            {
-                k = x * r + y;
-                arr[k] = TwoDArray[x,y];
-                k = k + 1;
-            }
-        }
+        
 
         Console.WriteLine("Los elementos del array bidimensional son: ");
         for (int x = 0; x < r; x++)
@@ -36,7 +28,7 @@ class Program
         {
             for (int y = 0; y < c; y++)
             {
-                Console.Write(arr[x*r+y]+" " );
+                Console.Write(TwoDArray[x,y]+" " );
             }
             
         }
