b0d10cc46401e2448c9c7bf4005097579c9579b4
castroojesus <jesusacs1111@gmail.com>
2025-09-18 14:13:35 -0700

    Create BubbleSort.java

---
+++ BubbleSort.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int []arr=new int[15];
+		Random rand = new Random();
+		
+		for (int i=0;i<arr.length;i++) {
+			arr[i]=rand.nextInt(100)+1;
+		}
+		
+		System.out.println("\nArray Original: ");
+		for (int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+		
+		int mayor=0;
+		for (int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length-i-1;j++) {
+				
+				if(arr[j]>arr[j+1]) {
+					mayor=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=mayor;
+				}
+			}
+		}
+		System.out.println("\nArray Ordenado: ");
+		for (int i=0;i<arr.length;i++) {
+			System.out.print(arr[i]+" ");
+		}
+
+	}
+
+}
+
